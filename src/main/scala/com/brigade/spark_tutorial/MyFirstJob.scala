8ef24c84e14b0963c59426568ac79e4c8669d863
Mark Cooper <mcooper@Marks-MBP.corp.brigade.zone>
2017-01-31 12:42:42 -0800

    Added base job code.

--- src/main/scala/com/brigade/spark_tutorial/MyFirstJob.scala
+++ src/main/scala/com/brigade/spark_tutorial/MyFirstJob.scala
@@ -1,8 +1,58 @@
 package com.brigade.spark_tutorial
 
-/**
-  * Created by mcooper on 1/31/17.
-  */
-class MyFirstJob {
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Dataset, SparkSession}
 
+
+case class CensusEntry(year: Int,
+                firstName: String,
+                county: String,
+                sex: String,
+                count: Int)
+
+class MyFirstJob(spark: SparkSession) {
+  import spark.implicits._
+
+  def run(input: String)  = {
+    //
+    // Read in the census file
+    //
+    val inputRDD: RDD[CensusEntry] =
+      spark
+        .read
+        .format("org.apache.spark.csv")
+        .option("header", "true")
+        .option("inferSchema", "true")
+        .csv(input)
+        .as[CensusEntry]
+        .rdd
+
+    //
+    // Let's print out the first five rows
+    //
+    inputRDD.take(5).foreach { censusEntry =>
+      println(censusEntry)
+    }
+
+    //
+    // Insert your code here:
+    //    * Total number of births
+    //    * Compute the ten most common names for boys and girls
+    //    * The percent breakdown by gender
+    //    * Number of births for the 10 largest cities
+  }
+}
+
+object MyFirstJob {
+  def main(args: Array[String]) {
+    val babyNamesInput = "data/BabyNamesNY.csv"
+    val spark = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .config("spark.some.config.option", "some-value")
+      .getOrCreate()
+
+    val job = new MyFirstJob(spark)
+    job.run(babyNamesInput)
+  }
 }
